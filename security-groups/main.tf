7e3423125f00beeeeed8c38d5a79b0862a6266e9
saude27 <akandesaudat2711@gmail.com>
2025-03-04 11:17:08 +0000

    create security group file

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -118,7 +118,7 @@ resource "aws_security_group" "database_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = -1
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
