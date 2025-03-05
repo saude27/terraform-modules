908b86505f056d39ddc25ad787575e21c7780930
saude27 <akandesaudat2711@gmail.com>
2025-03-05 12:20:23 +0000

    created s3 module

--- s3/variable.tf
+++ s3/variable.tf
@@ -1,8 +1,4 @@
 # s3 variables
 variable "project_name" {}
 variable "env_file_bucket_name" {}
-variable "env_file_name" {
-    description = "The name of the environment file to upload"
-    type = string
-    default = "env_file.env"
-}
+variable "env_file_name" {}
