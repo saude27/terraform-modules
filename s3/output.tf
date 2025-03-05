32f4947a3e569aeecc5dc5598fdbd31cfac048dc
saude27 <akandesaudat2711@gmail.com>
2025-03-05 14:38:39 +0000

    c

--- s3/output.tf
+++
@@ -1,9 +0,0 @@
-# export the s3 bucket name
-output "env_file_bucket_name" {
-  value = var.env_file_bucket_name
-}
-
-# export the environment file name
-output "env_file_name" {
-  value = var.env_file_name
-}
--- s3/variable.tf
+++
@@ -1,4 +0,0 @@
-# s3 variables
-variable "project_name" {}
-variable "env_file_bucket_name" {}
-variable "env_file_name" {}
